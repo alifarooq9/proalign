e4d94492b6f28611c0e807a714133e4e938423ab
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-09-10 18:07:37 +0500

    env

---
+++ env/server.ts
@@ -0,0 +1,11 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+    server: {
+        CLERK_SECRET_KEY: z.string().min(1),
+    },
+    runtimeEnv: {
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    },
+});
