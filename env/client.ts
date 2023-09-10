982a4a8df8035469ec8e1d5051b78147e5331bf6
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-09-10 18:30:46 +0500

    convex dev

--- env/client.ts
+++ env/client.ts
@@ -4,9 +4,11 @@ import { z } from "zod";
 export const env = createEnv({
     client: {
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+        NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
     },
     runtimeEnv: {
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
             process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+        NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
     },
 });
