eb883ed6092e99dddaf3cc80f579395819a451fb
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-09-13 22:01:31 +0500

    vercel build fix

--- app/(app)/(project)/project/[id]/details/loading.tsx
+++ app/(app)/(project)/project/[id]/details/loading.tsx
@@ -1,5 +1,9 @@
-import { ProjectDetailsPageloading } from "./page";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function DetailsPageLoading() {
-    return <ProjectDetailsPageloading />;
+    return (
+        <div className="container flex max-w-4xl items-center justify-center space-y-6 px-0 py-6">
+            <Skeleton className="h-96 w-full" />
+        </div>
+    );
 }
--- app/(app)/(project)/project/[id]/details/page.tsx
+++ app/(app)/(project)/project/[id]/details/page.tsx
@@ -41,7 +41,7 @@ export default function ProjectDetailsPage({ params }: ProjectDetailsProps) {
     );
 }
 
-export function ProjectDetailsPageloading() {
+function ProjectDetailsPageloading() {
     return (
         <div className="container flex max-w-4xl items-center justify-center space-y-6 px-0 py-6">
             <Skeleton className="h-96 w-full" />
