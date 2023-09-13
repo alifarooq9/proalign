402601ffc18cfe60e6dda332a234e9aedf87e40a
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-09-14 03:52:32 +0500

    page editor

--- app/(app)/project/[id]/page/[pageId]/page.tsx
+++ app/(app)/project/[id]/page/[pageId]/page.tsx
@@ -1,3 +1,13 @@
+import dynamic from "next/dynamic";
+const Editor = dynamic(() => import("@/components/editor"), {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+});
+
 export default function PageIdPage() {
-    return <div className="flex w-full justify-center "></div>;
+    return (
+        <div className="container my-6 flex w-full max-w-5xl justify-center">
+            <Editor />
+        </div>
+    );
 }
---
+++ components/editor.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import EditorJS from "@editorjs/editorjs";
+import { useCallback, useEffect, useRef, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { Button } from "@/components/ui/button";
+import useDebounce from "lodash.debounce";
+
+export default function Editor() {
+    const [isMounted, setIsMounted] = useState<boolean>(false);
+    const ref = useRef<EditorJS>();
+
+    const initializeEditor = useCallback(async () => {
+        const EditorJS = (await import("@editorjs/editorjs")).default;
+        const Header = (await import("@editorjs/header")).default;
+        const Embed = (await import("@editorjs/embed" as any)).default;
+        const Table = (await import("@editorjs/table" as any)).default;
+        const List = (await import("@editorjs/list" as any)).default;
+        const Code = (await import("@editorjs/code" as any)).default;
+        const LinkTool = (await import("@editorjs/link" as any)).default;
+        const InlineCode = (await import("@editorjs/inline-code" as any))
+            .default;
+        const Checklist = (await import("@editorjs/checklist" as any)).default;
+
+        if (!ref.current) {
+            const editor: EditorJS = new EditorJS({
+                holder: "editor",
+                onReady() {
+                    ref.current = editor;
+                },
+                placeholder: "Type here to write your post...",
+                inlineToolbar: true,
+                tools: {
+                    header: Header,
+                    linkTool: LinkTool,
+                    list: List,
+                    code: Code,
+                    inlineCode: InlineCode,
+                    table: Table,
+                    embed: Embed,
+                    checklist: Checklist,
+                },
+                onChange: (editor) => {
+                    debouncedSave();
+                },
+            });
+        }
+    }, []);
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setIsMounted(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (isMounted) {
+            initializeEditor();
+
+            return () => {
+                ref.current?.destroy();
+                ref.current = undefined;
+            };
+        }
+    }, [isMounted, initializeEditor]);
+
+    const debouncedSave = useDebounce(async () => {
+        const savedData = await ref.current?.saver.save();
+        console.log(savedData?.blocks);
+    }, 1000);
+
+    return (
+        <div className="w-full space-y-2">
+            <div className="flex items-center justify-end">
+                <Button variant="secondary">Save Changes</Button>
+            </div>
+            <TextareaAutosize
+                autoFocus
+                id="title"
+                defaultValue={"Test"}
+                placeholder="Post title"
+                className="w-full resize-none appearance-none overflow-hidden bg-transparent text-3xl font-bold focus:outline-none sm:text-5xl"
+            />
+            <div
+                id="editor"
+                className="prose dark:prose-invert dark:prose-neutral min-h-[600px] w-full max-w-5xl rounded-lg border-2 border-dashed bg-background p-2 sm:px-16 sm:py-6"
+            />
+        </div>
+    );
+}
